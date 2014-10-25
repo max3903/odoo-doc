53543a7528ee150589f92f8d8ff2eec9cfebfd69
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2014-10-25 08:27:34 -0400

    [ADD] Initial import

---
+++ doc_accounting_fr/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2010 - 2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Accounting Documentation in French',
+    'version': '0.1',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'license': 'AGPL-3',
+    'category': 'Others',
+    'summary': 'Documentation of the accounting module',
+    'description': """
+Accounting Documentation
+========================
+This module contains the documentation of the accounting modules.
+
+Contributors
+------------
+* Julien Roux (julien.roux@savoirfairelinux.com)
+* Maxime Chambreuil (maxime.chambreuil@savoirfairelinux.com)
+* Savoir-faire Linux (support@savoirfairelinux.com)
+""",
+    'depends': [
+        'account_voucher',
+        'doc_fr',
+    ],
+    'data': [
+        'data/doc_accounting.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
